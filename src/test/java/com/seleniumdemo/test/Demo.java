cf6a8fe893c8c5eb3380fbc7383cb7c43b9c19a8
vib <admin@hi>
2017-12-24 14:20:51 +0530

    done

---
+++ src/test/java/com/seleniumdemo/test/Demo.java
@@ -0,0 +1,15 @@
+package com.seleniumdemo.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.gecko.driver",
+				"F:\\Practise\\Selenium\\seleniumdemo\\src\\main\\resources\\exefiles\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://mvnrepository.com/");
+	}
+
+}
